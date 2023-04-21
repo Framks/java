87aa0b27dd142c25020b6d6ce780c7d5ddca0a87
Francisco <galvesa9@gmail.com>
2023-04-21 10:51:31 -0300

    atividade venda simples

---
+++ venda_simples/ItemDeVenda.java
@@ -0,0 +1,24 @@
+package venda_simples;
+
+public class ItemDeVenda {
+
+    String codigo;
+    String descricao;
+    Double quantidade;
+    String unidade;
+    Double valorUnitario;
+
+    public ItemDeVenda(String codigo, String descrisao, Double quantidade, String unidade, Double valorUnitario) {
+
+    }
+
+    String imprimir() {
+
+        return "";
+    }
+
+    Double getValorTotal() {
+
+        return 0.0;
+    }
+}
---
+++ venda_simples/Venda.java
@@ -0,0 +1,25 @@
+package venda_simples;
+
+public class Venda {
+
+    ItemDeVenda[] items;
+    String numeroNotaFiscal;
+
+    public Venda(String numeroNotaFiscal) {
+
+    }
+
+    String adicionarItem(String codigo, String descrisao, Double quantidade, String unidade, Double valorUnitario) {
+
+        return "";
+    }
+
+    Double getValorTotal() {
+
+        return 0.0;
+    }
+
+    String imprimir() {
+        return "";
+    }
+}
